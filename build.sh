eaecd0ce71b1a3f00a3ca83c4159462b102c25bd
jerb <jsilliman@protonmail.com>
2023-04-14 20:59:48 -0400

    experiment: tuigreet

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ cd /etc/yum.repos.d
 wget https://pkgs.tailscale.com/stable/fedora/tailscale.repo
 # don't forget to s/37/38 when we switch
 wget https://copr.fedorainfracloud.org/coprs/david35mm/pamixer/repo/fedora-37/david35mm-pamixer-fedora-37.repo
+wget https://copr.fedorainfracloud.org/coprs/alebastr/sway-extras/repo/fedora-37/alebastr-sway-extras-fedora-37.repo
 
 echo "-- Installing RPMs defined in recipe.yml --"
 rpm_packages=$(yq '.rpms[]' < /tmp/ublue-recipe.yml)
@@ -16,10 +17,9 @@ for pkg in $(echo -e "$rpm_packages"); do \
 done
 echo "---"
 
-# install yafti to install flatpaks on first boot, https://github.com/ublue-os/yafti
 pip install --prefix=/usr yafti
-
 pip install --prefix=/usr pywal
+
 
 # add a package group for yafti using the packages defined in recipe.yml
 yq -i '.screens.applications.values.groups.Custom.description = "Flatpaks defined by the image maintainer"' /etc/yafti.yml
