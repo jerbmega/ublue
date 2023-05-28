a1b33ca132f8c1c2a4dd9dcf8e24e9a08d602b9a
jerb <jsilliman@protonmail.com>
2023-05-27 23:43:59 -0400

    forgot to commit this one. oops

--- scripts/post/autorun.sh
+++ scripts/post/autorun.sh
@@ -3,11 +3,6 @@
 pip install --prefix=/usr pywal
 
 chmod +x /usr/etc/polybar/launch.sh
-chmod +x /usr/etc/ublue-lightdm-workaround.sh
-chmod +x /usr/etc/ublue-libvirt-workaround.sh
 
 systemctl enable lightdm
 systemctl enable tailscaled
-
-systemctl enable ublue-lightdm-workaround
-systemctl enable ublue-libvirt-workaround
