695f1d99ecdfb19ea817fc9ca6feb209f5b983a8
jerb <jsilliman@protonmail.com>
2023-06-19 09:42:23 -0400

    experiment: distrobox-assemble

--- scripts/post/autorun.sh
+++ scripts/post/autorun.sh
@@ -5,6 +5,16 @@ pip install --prefix=/usr pywal
 chmod +x /usr/etc/polybar/launch.sh
 chmod +x /usr/etc/ublue-lightdm-workaround.sh
 
+# Until Distrobox 1.5.0 lands in Fedora 38, install Distrobox manually.
+# Normally I would say this is disgusting but Distrobox is just a bunch of bash scripts so it's... fine?
+rpm-ostree override remove distrobox
+git clone https://github.com/89luca89/distrobox/releases/tag/1.5.0
+pushd distrobox
+git checkout main -b v1.5.0
+./install --prefix /usr/local
+popd
+yes | rm -r distrobox
+
 systemctl enable lightdm
 systemctl enable tailscaled
 
