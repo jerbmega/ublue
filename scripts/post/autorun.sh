ebd91c6efe5253be559bd7d000a00c799cfdae5d
Jeremy Silliman <jsilliman@protonmail.com>
2023-07-02 12:47:09 -0400

    refactor: go back to using distrobox rpm

--- scripts/post/autorun.sh
+++ scripts/post/autorun.sh
@@ -5,16 +5,6 @@ pip install --prefix=/usr pywal
 chmod +x /usr/etc/polybar/launch.sh
 chmod +x /usr/etc/ublue-lightdm-workaround.sh
 
-# Until Distrobox 1.5.0 lands in Fedora 38, install Distrobox manually.
-# Normally I would say this is disgusting but Distrobox is just a bunch of bash scripts so it's... fine?
-rpm-ostree override remove distrobox
-git clone https://github.com/89luca89/distrobox/
-pushd distrobox
-git checkout main -b v1.5.0.1
-./install --prefix /usr
-popd
-yes | rm -r distrobox
-
 systemctl enable lightdm
 systemctl enable tailscaled
 
